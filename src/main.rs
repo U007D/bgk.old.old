19ad4261591320cdb0d42966fbc7dca9830c6504
Brad Gibson <brad@humanenginuity.com>
2018-01-15 14:58:56 -0800

    green: rolling 1 + 10 yields 1 (illegal frame)

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,35 @@ extern crate polish;
 #[cfg(test)]
 mod unit_tests;
 
-struct Game {
+const MAX_PINS_PER_FRAME: u8 = 10;
+const ROLLS_PER_FRAME: usize = 2;
+const AWAITING_1ST_BALL: usize = 0;
+const AWAITING_2ND_BALL: usize = 1;
+
+pub struct Game {
     rolls: Vec<u8>,
 }
 
 impl Game {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Game { rolls: Vec::<u8>::new() }
     }
 
-    fn roll(&mut self, roll: u8) -> &mut Self {
-        if roll <= 10 {
+    pub fn roll(&mut self, roll: u8) -> &mut Self {
+        let pins_this_frame = match self.rolls.len() % ROLLS_PER_FRAME {
+            AWAITING_1ST_BALL => 0,
+            AWAITING_2ND_BALL => self.rolls[self.rolls.len() - 1],
+            _ => panic!("Internal error: ROLLS_PER_FRAME != 2"),
+        };
+
+        if roll <= MAX_PINS_PER_FRAME - pins_this_frame {
             self.rolls.push(roll);
         }
+
         self
     }
 
-    fn score(&self) -> Option<u8> {
+    pub fn score(&self) -> Option<u8> {
         match self.rolls.len() {
             0 => None,
             _ => Some(self.rolls.iter().sum())
