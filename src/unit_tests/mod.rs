1af07587a8e5491c2b90fb3edec5a67232771378
Brad Gibson <b2b@humanenginuity.com>
2018-05-10 12:09:05 -0700

    test no balls rolled: refactor

--- src/unit_tests/mod.rs
+++ src/unit_tests/mod.rs
@@ -1,46 +1,6 @@
 extern crate hesl;
 
+mod game;
+
+use game::Game;
 use rspec::{given, run};
-use Game;
-
-#[derive(Clone, Default, Debug)]
-struct Environment {
-    game: Game,
-    expected_result: u16,
-}
-
-#[test]
-fn score_tests() {
-    run(&given("a fresh game", Environment::default(), |ctx| {
-        ctx.before(|env| {
-            *env = Environment::default();
-        });
-
-        ctx.then("the score should be 0", |env| {
-            assert!(env.game.score() == env.expected_result);
-        });
-
-        ctx.when("a gutter ball is rolled", |ctx| {
-            ctx.before(|env| {
-                env.game.roll(0);
-                env.expected_result = 0;
-            });
-
-            ctx.then("the score should be 0", |env| {
-               assert!(env.game.score() == env.expected_result);
-            });
-        });
-
-        ctx.when("a pin is hit", |ctx| {
-            ctx.before(|env| {
-                env.game.roll(1);
-                env.expected_result = 1;
-            });
-
-            ctx.then("the score should be 1", |env| {
-                assert!(env.game.score() == env.expected_result);
-            });
-        });
-    }));
-}
-
