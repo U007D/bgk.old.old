6beae08fadefef742a9de83aa5b1ec2186d9763c
Brad Gibson <b2b@humanenginuity.com>
2018-05-09 22:26:10 -0700

    test no balls rolled: red

--- src/unit_tests/mod.rs
+++ src/unit_tests/mod.rs
@@ -5,11 +5,15 @@ use di::Container;
 
 #[test]
 fn tests() {
-    run(&given("", (), |ctx| {
+    run(&given("a game where no balls have been rolled", (), |ctx| {
+        let sut = Game::new();
+        let expected_result = 0;
 
-        ctx.when("", |ctx| {
+        ctx.when("the score is queried", |ctx| {
+             let result = sut.score();
 
             ctx.then("", move |_| {
+                assert!(result == expected_result);
             });
         });
     }));
