7366f005a4386db6c464a7eb42bc64028f082897
Brad Gibson <b2b@humanenginuity.com>
2018-05-11 17:07:40 -0700

    with no rolls, score should be 0: red

---
+++ src/unit_tests/game/score.rs
@@ -0,0 +1,25 @@
+use super::*;
+
+#[derive(Clone, Default, Debug)]
+struct Env {
+    game: Game,
+}
+
+#[test]
+fn tests() {
+    run(&given("an initialized Game instance", Env::default(), |ctx| {
+        let expected_result = 0;
+
+        ctx.then("with no rolls, score should be 0", move |env| {
+            assert!(env.game.score() == expected_result);
+        });
+
+//        ctx.when("", |ctx| {
+//            ctx.before(|env| {});
+//
+//            ctx.then("", |env| {
+//            });
+//        });
+    }));
+}
+
